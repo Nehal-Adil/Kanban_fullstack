b073f95784d5af121aa76ce4852f0965bf5a5d0e
Nehal Adil <mdnehaladil@gmail.com>
2026-07-07 12:06:47 +0530

    add imports in task module

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { KanbanColumn as Column } from '../columns/entities/column.entity';
+import { TaskAssignment } from './entities/task-assignment.entity';
+import { User } from '../users/entities/user.entity';
+import { Comment } from './entities/comment.entity';
+import { TasksService } from './tasks.service';
 
 @Module({
-  controllers: [TasksController]
+  imports: [
+    TypeOrmModule.forFeature([Task, Column, TaskAssignment, User, Comment]),
+  ],
+  providers: [TasksService],
+  controllers: [TasksController],
+  exports: [TasksService],
 })
 export class TasksModule {}
